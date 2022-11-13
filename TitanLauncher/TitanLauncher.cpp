706ddf9e0630ef38d5326562351cf5af3c93f8cc
Ghost <miningmaster28@gmail.com>
2022-11-13 17:54:43 -0600

    launcher setup

---
+++ TitanLauncher/TitanLauncher.cpp
@@ -0,0 +1,12 @@
+﻿// TitanLauncher.cpp : Defines the entry point for the application.
+//
+
+#include "TitanLauncher.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
