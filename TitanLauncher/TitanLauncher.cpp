d6b9dae932b8d146c60de3b1f3d992cad60922d0
Ghost <miningmaster28@gmail.com>
2022-11-14 13:56:24 -0600

    create window and setup tabs

--- TitanLauncher/TitanLauncher.cpp
+++ TitanLauncher/TitanLauncher.cpp
@@ -1,11 +1,8 @@
-﻿// wxWidgets "Hello World" Program
-
-// For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
-
-#ifndef WX_PRECOMP
+﻿#include <wx/wxprec.h>
 #include <wx/wx.h>
-#endif
+#include <wx/textctrl.h>
+#include <wx/bookctrl.h>
+#include <iostream>
 
 class MyApp : public wxApp
 {
@@ -16,66 +13,133 @@ public:
 class MyFrame : public wxFrame
 {
 public:
-    MyFrame();
-
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
+    wxBookCtrl* book;
+    wxListBox* listbox1;
+    wxTextCtrl* textLog;
+    MyFrame(const wxString& title);
+    void OnButton1(wxCommandEvent& event);
+    void OnButton2(wxCommandEvent& event);
+    void OnListBoxDoubleClick(wxCommandEvent& event);
+    void OnQuit(wxCommandEvent& event);
     void OnAbout(wxCommandEvent& event);
+private:
+    DECLARE_EVENT_TABLE()
 };
 
-enum
-{
-    ID_Hello = 1
-};
+// Declare some IDs. These are arbitrary.
+const int BOOKCTRL = 100;
+const int BUTTON1 = 101;
+const int BUTTON2 = 102;
+const int LISTBOX1 = 103;
+const int TEXTBOX1 = 104;
+const int FILE_QUIT = wxID_EXIT;
+const int HELP_ABOUT = wxID_ABOUT;
 
-wxIMPLEMENT_APP(MyApp);
+// Attach the event handlers. Put this after MyFrame declaration.
+BEGIN_EVENT_TABLE(MyFrame, wxFrame)
+EVT_BUTTON(BUTTON1, MyFrame::OnButton1)
+EVT_BUTTON(BUTTON2, MyFrame::OnButton2)
+EVT_LISTBOX_DCLICK(LISTBOX1,
+    MyFrame::OnListBoxDoubleClick)
+    EVT_MENU(FILE_QUIT, MyFrame::OnQuit)
+    EVT_MENU(HELP_ABOUT, MyFrame::OnAbout)
+    END_EVENT_TABLE()
 
-bool MyApp::OnInit()
+    IMPLEMENT_APP(MyApp)
+
+    bool MyApp::OnInit()
 {
-    MyFrame* frame = new MyFrame();
+    MyFrame* frame = new MyFrame(
+        _T("My wxWidgets Demo"));
     frame->Show(true);
     return true;
 }
 
-MyFrame::MyFrame()
-    : wxFrame(nullptr, wxID_ANY, "Hello World")
+MyFrame::MyFrame(const wxString& title)
+    : wxFrame(NULL, wxID_ANY, title, wxPoint(50, 30), wxSize(960, 540))
 {
-    wxMenu* menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-        "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-
-    wxMenu* menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
-
-    wxMenuBar* menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
-
+    SetIcon(wxICON(sample));
+    wxMenu* fileMenu = new wxMenu;
+    wxMenu* helpMenu = new wxMenu;
+    helpMenu->Append(HELP_ABOUT, _T("&About...\tF1"),
+        _T("Show about dialog"));
+    fileMenu->Append(FILE_QUIT, _T("E&xit\tAlt-X"),
+        _T("Quit this program"));
+    wxMenuBar* menuBar = new wxMenuBar();
+    menuBar->Append(fileMenu, _T("&File"));
+    menuBar->Append(helpMenu, _T("&Help"));
     SetMenuBar(menuBar);
+    CreateStatusBar(2);
+    SetStatusText(_T("So far so good."), 0);
+    SetStatusText(_T("Welcome."), 1);
 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    book = new wxBookCtrl(this, BOOKCTRL);
+    wxPanel* panel = new wxPanel(book);
+    new wxButton(panel, BUTTON1,
+        _T("Button &1"), wxPoint(50, 30), wxSize(100, 30));
+    new wxButton(panel, BUTTON2,
+        _T("Button &2"), wxPoint(50, 80), wxSize(100, 30));
+    book->AddPage(panel, _T("Game"), true);
 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+    wxString choices[] =
+    {
+     _T("720x480"),
+     _T("960x540"),
+     _T("1280x720"),
+     _T("1366x768"),
+     _T("1600x900"),
+     _T("1920x1080"),
+     _T("2560x1440"),
+     _T("3200x1800"),
+     _T("3840x2160"),
+    };
+
+    panel = new wxPanel(book);
+    listbox1 = new wxListBox(panel, LISTBOX1,
+        wxPoint(25, 25), wxSize(150, 150),
+        9, choices, wxLB_SINGLE);
+    book->AddPage(panel, _T("Display"), false);
+
+
+    panel = new wxPanel(book);
+    wxBoxSizer* mysizer = new wxBoxSizer(wxVERTICAL);
+    panel->SetSizer(mysizer);
+    textLog = new wxTextCtrl(panel, TEXTBOX1, _T("Log\n"),
+        wxPoint(0, 250), wxSize(100, 50), wxTE_MULTILINE);
+    mysizer->Add(textLog, 1, wxEXPAND | wxALL, 5);
+    book->AddPage(panel, _T("Debug"), false);
 }
 
-void MyFrame::OnExit(wxCommandEvent& event)
+void MyFrame::OnQuit(wxCommandEvent& WXUNUSED(event))
 {
     Close(true);
 }
 
-void MyFrame::OnAbout(wxCommandEvent& event)
+void MyFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
 {
-    wxMessageBox("This is a wxWidgets Hello World example",
-        "About Hello World", wxOK | wxICON_INFORMATION);
+    wxString msg;
+    msg.Printf(_T("About.\n")
+        _T("Welcome to %s"), wxVERSION_STRING);
+
+    wxMessageBox(msg, _T("About My Program"),
+        wxOK | wxICON_INFORMATION, this);
 }
 
-void MyFrame::OnHello(wxCommandEvent& event)
+void MyFrame::OnButton1(wxCommandEvent& WXUNUSED(event))
 {
-    wxLogMessage("Hello world from wxWidgets!");
+    wxMessageBox("Click1", "Click",
+        wxOK | wxICON_INFORMATION, this);
+}
+
+void MyFrame::OnButton2(wxCommandEvent& WXUNUSED(event))
+{
+    wxMessageBox("Click2", "Click",
+        wxOK | wxICON_INFORMATION, this);
+}
+
+void MyFrame::OnListBoxDoubleClick(wxCommandEvent& event)
+{
+    *textLog << "ListBox double click string is: \n";
+    *textLog << event.GetString();
+    *textLog << "\n";
 }
