dde5285b28da884d39bd6682cb204bf96654b318
Ghost <miningmaster28@gmail.com>
2022-11-13 19:49:03 -0600

    added libraries

--- TitanLauncher/TitanLauncher.cpp
+++ TitanLauncher/TitanLauncher.cpp
@@ -5,8 +5,29 @@
 
 using namespace std;
 
+void TitanLauncher::Setup()
+{
+	glfwInit();
+
+	glfwWindowHint(GLFW_RESIZABLE, false);
+	window = glfwCreateWindow(1280, 720, "Titan Launcher", NULL, NULL);
+}
+
+void TitanLauncher::Run()
+{
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	cout << "Hello Titan." << endl;
+	TitanLauncher* launcher = new TitanLauncher();
+
+	launcher->Setup();
+	launcher->Run();
 	return 0;
 }
--- TitanLauncher/TitanLauncher.h
+++ TitanLauncher/TitanLauncher.h
@@ -5,4 +5,18 @@
 
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+#include <GLFW/glfw3native.h>
+
+#include <nlohmann/json.hpp>
+
+class TitanLauncher
+{
+public:
+	void Setup();
+	void Run();
+private:
+	GLFWwindow* window;
+};
+
 // TODO: Reference additional headers your program requires here.
